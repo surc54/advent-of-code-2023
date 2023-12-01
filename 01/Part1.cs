0dbca4288dc22e3fc56ac4e9ae71773513722720
Adithya Haridas <adithya.haridas@infotechinc.com>
2023-12-01 17:26:55 -0500

    Clean up day 1 code

--- 01/Part1.cs
+++ 01/Part1.cs
@@ -1,40 +1,35 @@
+namespace _01;
+
 internal static class Part1
 {
-  internal static async Task Run(string[] args)
-  {
-    var fileName = args[0] ?? throw new ArgumentNullException("args[0]", "Specify the input file as the first argument.");
-    if (!File.Exists(fileName)) throw new FileNotFoundException($"Could not find file {fileName}");
-
-    var data = await File.ReadAllTextAsync(fileName);
-    var lines = data.Split('\n', '\r').Where(line => !string.IsNullOrWhiteSpace(line));
-
-    long sum = 0;
-
-    foreach (var line in lines)
+    public static long Run(IEnumerable<string> lines)
     {
-      var number = GetFirstAndLastDigitsCombined(line);
-      sum += number;
+        long sum = 0;
+
+        foreach (var line in lines)
+        {
+            var number = GetFirstAndLastDigitsCombined(line);
+            sum += number;
+        }
+
+        return sum;
     }
 
-    Console.WriteLine("Sum: {0}", sum);
-  }
-
-
-  static int GetFirstAndLastDigitsCombined(string text)
-  {
-    char? firstDigit = null;
-    char? lastDigit = null;
-
-    foreach (var character in text)
+    public static int GetFirstAndLastDigitsCombined(string text)
     {
-      if (!char.IsDigit(character)) continue;
+        char? firstDigit = null;
+        char? lastDigit = null;
 
-      firstDigit ??= character;
-      lastDigit = character;
+        foreach (var character in text)
+        {
+            if (!char.IsDigit(character)) continue;
+
+            firstDigit ??= character;
+            lastDigit = character;
+        }
+
+        if (firstDigit is null || lastDigit is null) throw new ArgumentException("Provided text does not have digits");
+
+        return int.Parse($"{firstDigit}{lastDigit}");
     }
-
-    if (firstDigit is null || lastDigit is null) throw new ArgumentException("Provided text does not have digits");
-
-    return int.Parse($"{firstDigit}{lastDigit}");
-  }
 }
--- 01/Program.cs
+++ 01/Program.cs
@@ -1,1 +1,12 @@
-﻿await Part2.Run(args);
+﻿using _01;
+
+var fileName = args.Length > 0
+    ? args[0]
+    : throw new InvalidOperationException("File name must be provided as the first argument");
+if (!File.Exists(fileName)) throw new FileNotFoundException($"Could not find file {fileName}");
+
+var data = await File.ReadAllTextAsync(fileName);
+var lines = data.Split('\n', '\r').Where(line => !string.IsNullOrWhiteSpace(line));
+
+Console.WriteLine("Part 1: {0}", Part1.Run(lines));
+Console.WriteLine("Part 2: {0}", Part2.Run(lines));
